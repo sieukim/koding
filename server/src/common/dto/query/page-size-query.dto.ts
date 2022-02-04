a8c0281310127d547c443725a40af13d1f86ce04
vvsos1 <vvsos10@naver.com>
2022-02-05 04:27:54 +0900

    페이징 API 의 페이지 사이즈 오류 수정

--- server/src/common/dto/query/page-size-query.dto.ts
+++ server/src/common/dto/query/page-size-query.dto.ts
@@ -1,13 +1,14 @@
-import { IsNumber, Max, Min } from "class-validator";
+import { Max, Min } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
 
 export class PageSizeQueryDto {
   @Min(1)
   @Max(30)
-  @IsNumber()
   @ApiProperty({
     description: "페이지당 개수",
     type: Number,
   })
+  @Transform(({ value }) => parseInt(value), { toClassOnly: true })
   pageSize?: number = 10;
 }
