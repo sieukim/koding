c2d217ac271aab3ce1a4caaa42e8bf6b2834a932
sieukim <s2lver@naver.com>
2022-02-13 00:25:47 +0900

    search 리팩터링

---
+++ web-client/src/components/presenters/styled/search/StyledSearchPage.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledSearchPage = styled.div`
+  border: 1px solid rgb(217, 217, 217);
+  border-radius: 8px;
+  margin: 50px 0;
+  padding: 32px;
+  width: 900px;
+
+  .search-icon {
+    font-size: 1rem;
+    color: rgba(0, 0, 0, 0.45);
+  }
+
+  .result-card {
+    margin: 30px 0;
+  }
+`;
