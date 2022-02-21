87e1293eefde3751630062ffa377dde5b32fac19
vvsos1 <vvsos10@naver.com>
2022-02-22 04:06:56 +0900

    누락된 쿼리 핸들러 등록

--- server/src/posts/query/handlers/index.ts
+++ server/src/posts/query/handlers/index.ts
@@ -2,14 +2,14 @@ import { GetPostListHandler } from "./get-post-list.handler";
 import { ReadPostHandler } from "./read-post.handler";
 import { CheckUserLikePostHandler } from "./check-user-like-post.handler";
 import { GetDailyRankingHandler } from "./get-daily-ranking.handler";
-import { CheckUserScrapPostQuery } from "../check-user-scrap-post.query";
-import { GetPostsOfFollowingsQuery } from "../get-posts-of-followings.query";
+import { CheckUserScrapPostHandler } from "./check-user-scrap-post.handler";
+import { GetPostsOfFollowingsHandler } from "./get-posts-of-followings.handler";
 
 export const PostQueryHandlers = [
   GetPostListHandler,
   ReadPostHandler,
   CheckUserLikePostHandler,
   GetDailyRankingHandler,
-  CheckUserScrapPostQuery,
-  GetPostsOfFollowingsQuery,
+  CheckUserScrapPostHandler,
+  GetPostsOfFollowingsHandler,
 ];
